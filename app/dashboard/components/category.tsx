814680c3a2f9a9c61bc2d41ed9ce04e6507f81a0
Suryadev78 <pandeysuryadev00@gmail.com>
2024-09-30 23:44:03 +0530

    adding catofy section to the page

--- app/dashboard/components/category.tsx
+++ app/dashboard/components/category.tsx
@@ -2,6 +2,6 @@ export interface Category {
   name: string;
   value: string;
 }
-export default function Category({ item }: { item: Category }) {
+export default function Category({ item }: { item: Category[] }) {
   return <div>Category</div>;
 }
