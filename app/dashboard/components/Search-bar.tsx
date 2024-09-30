0fe1d96fb4111944cb9c683a981dac3d6fb871a8
Suryadev78 <pandeysuryadev00@gmail.com>
2024-10-01 00:55:54 +0530

    added category layout

--- app/dashboard/components/Search-bar.tsx
+++ app/dashboard/components/Search-bar.tsx
@@ -1,6 +1,33 @@
 "use client";
 import { Auth } from "@/app/components/Auth";
 import { SearchIcon } from "lucide-react";
+import CategoryComponent from "./Category";
+const categories = [
+  {
+    name: "All",
+    value: "All",
+  },
+  {
+    name: "Youtube",
+    value: "Youtube",
+  },
+  {
+    name: "Tiktok",
+    value: "Tiktok",
+  },
+  {
+    name: "LinkedIn",
+    value: "LinkedIn",
+  },
+  {
+    name: "Instagram",
+    value: "Instagram",
+  },
+  {
+    name: "Twitter",
+    value: "Twitter",
+  },
+];
 
 export default function SearchBar({
   onSearchInput,
@@ -8,9 +35,9 @@ export default function SearchBar({
   onSearchInput: React.Dispatch<React.SetStateAction<string>>;
 }) {
   return (
-    <div className="mx-5 py-">
-      <div className="flex justify-between md:flex-row px-4 py-4">
-        <div className="flex gap-2 items-center py-1  bg-white border rounded-full w-full md:w-[30%]">
+    <div className="mx-5 py-2">
+      <div className="flex justify-between  md:flex-row px-4 py-4">
+        <div className="flex gap-2 items-center py-1 bg-white border rounded-full w-full md:w-[30%]">
           <SearchIcon className="ml-1" />
           <input
             onChange={(e) => onSearchInput(e.target.value)}
@@ -18,6 +45,9 @@ export default function SearchBar({
             placeholder="Search..."
             className="bg-transparent w-full outline-none "
           />
+        </div>
+        <div className="mx-4 bg-white  px-1 rounded-lg flex justify-center">
+          <CategoryComponent item={categories} />
         </div>
         <div>
           <Auth />
--- app/dashboard/components/category.tsx
+++ app/dashboard/components/category.tsx
@@ -2,6 +2,14 @@ export interface Category {
   name: string;
   value: string;
 }
-export default function Category({ item }: { item: Category[] }) {
-  return <div>Category</div>;
+export default function CategoryComponent({ item }: { item: Category[] }) {
+  return (
+    <div className="flex justify-center items-center">
+      {item.map((item) => (
+        <div key={item.value} className="mr-4">
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
 }
