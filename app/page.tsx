79386d1eb6dcc177c95ebe1cc921edcd18e3ca5d
Suryadev78 <pandeysuryadev00@gmail.com>
2024-10-06 23:26:02 +0530

    removed terms and privacy from footer

--- app/page.tsx
+++ app/page.tsx
@@ -97,14 +97,6 @@ export default function LandingPage() {
         <p className="text-xs text-gray-500 dark:text-gray-400">
           © 2024 StartupX Inc. All rights reserved.
         </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6 space-x-4">
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Privacy
-          </Link>
-        </nav>
       </footer>
     </div>
   );
